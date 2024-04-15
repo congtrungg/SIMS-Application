1944b55a7eb39f5ba2f7fb54f0d22a4ca5ee7d23
congtrungg <ctrpham.317@gmail.com>
2024-04-16 02:01:35 +0700

    SIMS

---
+++ SIMS_Demo/SIMS_TEST/Student.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using SIMS_Demo.Controllers;
+using SIMS_Demo.Models;
+using System.Collections.Generic;
+
+namespace SIMS_TEST
+{
+    public class StudentControllerTests
+    {
+        [Fact]
+        public void Index_ShouldReturnCorrectNumberOfStudents()
+        {
+            // Arrange
+            var controller = new StudentController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+            var students = result.Model as List<Student>;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(3, students.Count);
+        }
+    }
+}
